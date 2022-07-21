02bcf33941a348f5aca67b82f0face5e647c329a
Pedro Barbosa Muniz <pedro.barbosa@clinicasim.com>
2022-07-21 16:00:46 -0300

    Implementing Clean Architecture and Entities classes

---
+++ Domain/Entities/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDePedidos.Domain.Entities
+{
+    public sealed class Supplier: Entity
+    {
+        public string CompanyName { get; set; }
+        public string Cnpj { get; set; }
+        public string Uf { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactName { get; set; }
+    }
+}
